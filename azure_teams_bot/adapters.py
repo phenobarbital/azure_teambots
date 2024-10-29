a914743af1af7081c243448850fa6459b5e4b3e4
Jesus Lara <jesuslarag@gmail.com>
2024-10-29 01:18:25 +0100

    working on new version of Azurebots

--- azure_teams_bot/adapters.py
+++ azure_teams_bot/adapters.py
@@ -1,9 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-from collections.abc import Callable, Awaitable
-import sys
-import traceback
-import logging
 from datetime import datetime
 from botbuilder.core import (
     ConversationState,
@@ -15,6 +11,7 @@ from botbuilder.integration.aiohttp import (
     ConfigurationBotFrameworkAuthentication
 )
 from botbuilder.schema import ActivityTypes, Activity
+from navconfig.logging import logging
 from .config import BotConfig
 
 
@@ -27,8 +24,8 @@ class AdapterHandler(CloudAdapter):
         logger: logging.Logger,
         conversation_state: ConversationState,
     ):
-        self.config = config
-        self.logger = logger
+        self.config: BotConfig = config
+        self.logger: logging.Logger = logger
         settings = ConfigurationBotFrameworkAuthentication(
             self.config,
             logger=self.logger
@@ -40,55 +37,39 @@ class AdapterHandler(CloudAdapter):
         super().__init__(settings)
         self._conversation_state = conversation_state
 
-        # Catch-all for errors.
-        async def on_error(context: TurnContext, error: Exception):
-            # This check writes out errors to console log
-            # NOTE: In production environment,
-            # you should consider logging this to Azure
-            # application insights.
-            print(
-                f"\n [on_turn_error] unhandled error: {error}",
-                file=sys.stderr
-            )
-            traceback.print_exc()
+    # Catch-all for errors.
+    async def on_error(self, context: TurnContext, error: Exception):
+        # This check writes out errors to console log
+        # NOTE: In production environment,
+        # you should consider logging this to Azure
+        # application insights.
+        self.logger.error(
+            f"[on_turn_error] Unhandled error: {error}",
+            exc_info=True
+        )
 
-            # Send a message to the user
-            await context.send_activity("The bot encountered an error or bug.")
-            await context.send_activity(
-                "To continue to run this bot, please fix the bot source code."
+        # Send a message to the user
+        await context.send_activity(
+            "The bot encountered an error or bug."
+        )
+        await context.send_activity(
+            "To continue to run this bot, please fix the bot source code."
+        )
+        # Send a trace activity if we're talking to the
+        # Bot Framework Emulator
+        if context.activity.channel_id == "emulator":
+            # Create a trace activity that contains the error object
+            trace_activity = Activity(
+                label="TurnError",
+                name="on_turn_error Trace",
+                timestamp=datetime.utcnow(),
+                type=ActivityTypes.trace,
+                value=f"{error}",
+                value_type="https://www.botframework.com/schemas/error",
             )
-            # Send a trace activity if we're talking to the
+            # Send a trace activity, which will be displayed in
             # Bot Framework Emulator
-            if context.activity.channel_id == "emulator":
-                # Create a trace activity that contains the error object
-                trace_activity = Activity(
-                    label="TurnError",
-                    name="on_turn_error Trace",
-                    timestamp=datetime.utcnow(),
-                    type=ActivityTypes.trace,
-                    value=f"{error}",
-                    value_type="https://www.botframework.com/schemas/error",
-                )
-                # Send a trace activity, which will be displayed in
-                # Bot Framework Emulator
-                await context.send_activity(trace_activity)
+            await context.send_activity(trace_activity)
 
-            # Clear out state
-            nonlocal self
-            await self._conversation_state.delete(context)
-
-        self.on_turn_error = on_error
-
-    # async def process_activity(
-    #     self,
-    #     auth_header: str,
-    #     body: dict,
-    #     on_turn: Callable[[TurnContext], Awaitable]
-    # ):
-    #     activity = Activity().deserialize(body)
-    #     turn_context = TurnContext(self, activity)
-    #     turn_context.turn_state["http_status"] = 200  # Default to 200, adjust as necessary
-
-    #     await self.on_turn(turn_context)
-    #     await self.conversation_state.save_changes(turn_context)
-    #     return turn_context.turn_state.get("http_status", 200)
+        # Clear out state
+        await self._conversation_state.delete(context)
